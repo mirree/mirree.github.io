9dc2b1a514179237e63f8f06e524ee40dedbd6f4
mirree <32240143+mirree@users.noreply.github.com>
2020-11-24 14:40:40 +0100

    Updates

--- src/utils/answers.js
+++
@@ -1,9 +0,0 @@
-import four from "../assets/images/four.png";
-import three from "../assets/images/three.png";
-import seven from "../assets/images/seven.png";
-
-export const imgs = [
-  { img: seven, ans: 7, FFNNans: 1, dropoutAns: 7, BNNans: 7 },
-  { img: three, ans: 3, FFNNans: 8, dropoutAns: 3, BNNans: "No class selected" },
-  { img: four, ans: 4, FFNNans: 4, dropoutAns: 4, BNNans: 4 },
-];
